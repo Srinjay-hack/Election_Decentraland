72231f19e0fe6fc858913d937fe75c0d384fd606
Srinjay-hack <srinjaymondal@gmail.com>
2021-12-23 01:07:37 +0530

    decentraland

---
+++ contracts/Election.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22;
+contract Election{
+    //Store Candidate name
+
+    //Read candidate name
+    
+
+
+    //Constructor
+    string public candidate;
+    function Elections () public {
+        candidate = 'Candidate 1';
+    }
+}
